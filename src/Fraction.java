1fd2bbb2ed09c207f4b0f21543fd0f800ddecd87
koussaila <da700890@gmail.com>
2020-10-20 18:19:00 +0200

    tester la fraction

--- src/Fraction.java
+++ src/Fraction.java
@@ -1,4 +1,13 @@
 
 public class Fraction {
+	private int Numerateur;
+	private int Denominateur ;
 
+	public Fraction (int Numerateur, int Denominateur) {
+	this.Numerateur = Numerateur ;
+	this.Denominateur = Denominateur ;	
+	}
+	public String ToString() {
+		return this.Numerateur + "/" + this.Denominateur ;	
+	}	
 }
--- src/Main.java
+++ src/Main.java
@@ -2,8 +2,9 @@
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("hello Wolrd");
+		Fraction F = new Fraction(5,9);
+		System.out.println(F.ToString());
+		
 
 	}
 
